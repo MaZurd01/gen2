55eb88a05eae45b065ab75f7546b6cf2c932ed21
MaZurd01 <42522353+MaZurd01@users.noreply.github.com>
2018-12-23 22:54:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,175 +6,97 @@ const client4 = new Discord.Client();
 
 
 
-client.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`Generator Credits `);
-   console.log(`----------------`);
-   console.log(`Loadinng`);
-   console.log(`Loadinng.`);
-   console.log(`Loadinng..`);
-   console.log(`Loadinng...`);
-   console.log(`This Bots Online ' `);
-   console.log(`----------------`);
-});
 
 client.on('message', message => {
-    if(message.content === '-hey'){
-        message.channel.send('مفعل : :white_check_mark:')
-    }
-});
-client2.on('message', message => {
-    if(message.content === '-hey'){
-        message.channel.send('مفعل : :white_check_mark:')
-    }
-});
-client3.on('message', message => {
-    if(message.content === '-hey'){
-        message.channel.send('مفعل : :white_check_mark:')
-    }
-});
-
-client4.on('message', message => {
-    if(message.content === '-hey'){
-        message.channel.send('مفعل : :white_check_mark:')
-    }
-});
-
-
-      
-
-client.on('message', message => {
-
-   let command = message.content.split(" ")[0];
-  let args = message.content.split(" ").slice(1);
-  if(!args) return message.channel.send(`say5 <words>`);
-if (command == "say5") {
-message.channel.send(args.join("  "))
-   
-  }
-  
-});
-
-
-client2.on('message', message => {
-
-   let command = message.content.split(" ")[0];
-  let args = message.content.split(" ").slice(1);
-  if(!args) return message.channel.send(`say6 <words>`);
-if (command == "say6") {
-message.channel.send(args.join("  "))
-   
-  }
-  
-});
-
-
-client3.on('message', message => {
-
-   let command = message.content.split(" ")[0];
-  let args = message.content.split(" ").slice(1);
-  if(!args) return message.channel.send(`say7 <words>`);
-if (command == "say7") {
-message.channel.send(args.join("  "))
-   
-  }
-  
-});
-
-client4.on('message', message => {
-
-   let command = message.content.split(" ")[0];
-  let args = message.content.split(" ").slice(1);
-  if(!args) return message.channel.send(`say8 <words>`);
-if (command == "say8") {
-message.channel.send(args.join("  "))
-   
-  }
-});
-
-
-
-client.on('message', message => {
-    if(message.content === '-راتب'){
+    if(message.content === '$مرتبك'){
         message.channel.send('#daily')
     }
 });
 
 client.on('message', message => {
-    if(message.content === '-مبلغ'){
+    if(message.content === '$رصيدك'){
         message.channel.send('#credits')
     }
 });
 
 client.on('message', message => {
-    if(message.content === '-ريب'){
+    if(message.content === '$ريب'){
         message.channel.send('#rep <@511260487397408768>')
     }
 });
 
+
+
+
 client2.on('message', message => {
-    if(message.content === '-راتب'){
-        message.channel.send('#daily ')
+    if(message.content === '$مرتبك'){
+        message.channel.send('#daily')
     }
 });
 
 client2.on('message', message => {
-    if(message.content === '-مبلغ'){
+    if(message.content === '$رصيدك'){
         message.channel.send('#credits')
     }
 });
 
 client2.on('message', message => {
-    if(message.content === '-ريب'){
+    if(message.content === '$ريب'){
         message.channel.send('#rep <@511260487397408768>')
     }
 });
 
 
+
+
+
 client3.on('message', message => {
-    if(message.content === '-راتب'){
+    if(message.content === '$مرتبك'){
         message.channel.send('#daily')
     }
 });
 
 client3.on('message', message => {
-    if(message.content === '-مبلغ'){
+    if(message.content === '$رصيدك'){
         message.channel.send('#credits')
     }
 });
 
 client3.on('message', message => {
-    if(message.content === '-ريب'){
+    if(message.content === '$ريب'){
         message.channel.send('#rep <@511260487397408768>')
     }
 });
 
+
+
 client4.on('message', message => {
-    if(message.content === '-راتب'){
+    if(message.content === '$مرتبك'){
         message.channel.send('#daily')
     }
 });
 
 client4.on('message', message => {
-    if(message.content === '-مبلغ'){
+    if(message.content === '$رصيدك'){
         message.channel.send('#credits')
     }
 });
 
 client4.on('message', message => {
-    if(message.content === '-ريب'){
+    if(message.content === '$ريب'){
         message.channel.send('#rep <@511260487397408768>')
     }
 });
 
 
 
-client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
+
+client.on('message', message => { 
+if (message.content === '!startgen') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**Generator Credits, By MaZurd, Generator Credits, By Mazurd, Generator - توليد كريديت مجاني , بواسطه مازورد مصنع الكريديت ** ["${x}"]`)
           .then(m => {
             count++;
           })
@@ -183,12 +105,12 @@ if (message.content === '!spam') {
       }
 });
 
-client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
+client2.on('message', message => { 
+if (message.content === '!startgen') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**Generator Credits, By MaZurd, Generator Credits, By Mazurd, Generator - توليد كريديت مجاني , بواسطه مازورد مصنع الكريديت ** ["${x}"]`)
           .then(m => {
             count++;
           })
@@ -197,12 +119,12 @@ if (message.content === '!spam') {
       }
 });
 
-client3.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-    if (message.content === '!spam') {
+client3.on('message', message => { 
+    if (message.content === '!startgen') {
           let count = 0;
           let ecount = 0;
           for(let x = 0; x < 90000; x++) {
-            message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+            message.channel.send(`**Generator Credits, By MaZurd, Generator Credits, By Mazurd, Generator - توليد كريديت مجاني , بواسطه مازورد مصنع الكريديت ** ["${x}"]`)
               .then(m => {
                 count++;
               })
@@ -212,24 +134,24 @@ client3.on('message', message => { // لا تغير شئ عشان ما تخرب 
     });
 
 
-client4.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-    if (message.content === '!spam') {
-          let count = 0;
-          let ecount = 0;
-          for(let x = 0; x < 90000; x++) {
-            message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-              .then(m => {
-                count++;
-              })
-              
-            }
-          }
-    });
+    client4.on('message', message => { 
+        if (message.content === '!startgen') {
+              let count = 0;
+              let ecount = 0;
+              for(let x = 0; x < 90000; x++) {
+                message.channel.send(`**Generator Credits, By MaZurd, Generator Credits, By Mazurd, Generator - توليد كريديت مجاني , بواسطه مازورد مصنع الكريديت ** ["${x}"]`)
+                  .then(m => {
+                    count++;
+                  })
+                  
+                }
+              }
+        });
 
 
 
 
-client.login(process.env.TOKEN);// لا تغير فيها شيء
-client2.login(process.env.TOKEN2);// لا تغير فيها شيء
+client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN2);
 client3.login(process.env.TOKEN3);
 client4.login(process.env.TOKEN4);
