9bf66a639a09752a614364a30b1d3efb322a0682
MaZurd01 <42522353+MaZurd01@users.noreply.github.com>
2018-12-23 19:08:02 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,140 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+
+client.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`Generator Credits `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
+});
+
+
+client.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+
+client3.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+
+client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client3.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+    if (message.content === '!spam') {
+          let count = 0;
+          let ecount = 0;
+          for(let x = 0; x < 90000; x++) {
+            message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+              .then(m => {
+                count++;
+              })
+              
+            }
+          }
+    });
+
+
+
+
+client.login(process.env.TOKEN);// لا تغير فيها شيء
+client2.login(process.env.TOKEN2);// لا تغير فيها شيء
+client3.login(process.env.TOKEN3);
+
+
+
+//حسابات
+
+// 1- jqd55872@iencm.com - NTI1MzAyNjkxNjM1NjU4NzYy.DwA65A.hDN_bVhtu6kgkLmsVDq2d-I4_Jg
+// p- 123456789
+// 2- ltt35156@iencm.com - NTI2MTUwMjg4MzY1ODQ2NTMw.DwBARQ.4cXmHHOXZb_ypIMoOzECwwuIep4
+// 3- anr23171@iencm.com - NTI2MTUyNDMyMTQ1NzkzMDQ0.DwDHbQ.DlVFn9HRqOE2aZOjDMPmdhlbJbg
+// 4- oqq52966@ebbob.com - NTI2MzA0OTA3MDI4ODU2ODM2.DwDPoQ._qzLrPlI5pftppHD4niKCY1Bluc
+// 5- pwg62639@ebbob.com - NTI2MzA1ODU4NDY1MDM4MzU2.DwFNBw._lhwRlZAPhH2Ieaqs0GgEf8cg6c
+// 6- wri77613@ebbob.com - NTI2NDQyMzc3MTc1NjI5ODM0.DwFPxQ.5uYFnonoyTQz4YIkXEA2MkDtce8
+
+
+
+// TAG 9929
+// 1- uva47821@iencm.com
+// p- 123456789
